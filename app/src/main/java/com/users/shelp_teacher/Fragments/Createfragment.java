9dd5d27d354dfd8c0a80954429c42e914ffa847c
garima652001 <garima1913076@akgec.ac.in>
2020-10-05 16:47:18 +0530

    Added fragments and nav

---
+++ app/src/main/java/com/users/shelp_teacher/Fragments/Createfragment.java
@@ -0,0 +1,21 @@
+package com.users.shelp_teacher.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.users.shelp_teacher.R;
+
+public class Createfragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_createfragment, container, false);
+    }
+}
--- app/src/main/java/com/users/shelp_teacher/MainActivity.java
+++ app/src/main/java/com/users/shelp_teacher/MainActivity.java
@@ -1,18 +1,53 @@
 package com.users.shelp_teacher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.users.shelp_teacher.Fragments.Createfragment;
+import com.users.shelp_teacher.Fragments.Homefragment;
+import com.users.shelp_teacher.Fragments.Profilefragment;
 
+public class MainActivity extends AppCompatActivity {
+    ChipNavigationBar chipNavigationBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkuser();
+
+        chipNavigationBar = findViewById(R.id.bottom_nav);
+        chipNavigationBar.setItemSelected(R.id.nav_home,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new Homefragment()).commit();
+        menu();
     }
+
+    private void menu() {
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment=null;
+                switch(i){
+                    case R.id.nav_home:
+                        fragment= new Homefragment();
+                        break;
+
+                    case R.id.nav_profile:
+                        fragment= new Profilefragment();
+                        break;
+
+                    case R.id.nav_create:
+                        fragment= new Createfragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+            }
+        });
+    }
+
     public void checkuser()
     {
         Boolean check= Boolean.valueOf(Sharedprefs.readShared(MainActivity.this,"Clip","true"));
